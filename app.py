8dd35b0915d17e925a38c3d28b20a225a068d338
bfgdigital <ben@bfg.digital>
2020-11-11 14:59:44 +1100

    Rebuilt the whole thing

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import flask
 from flask import Flask, send_file, request, render_template, make_response, session
 
 # SQL
-import sqlalchemy
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
+# import sqlalchemy
+# from sqlalchemy import create_engine
+# from flask_sqlalchemy import SQLAlchemy
 
 # Redis
 import redis
@@ -55,7 +55,7 @@ except ImportError:
 # │   ├── /records
 # │   │   └── all recorded images...
 # │   ├── temp.png
-# │   ├── pallets_dictionary.csv
+# │   ├── PALLETS_DICT.csv
 # │   └── welcome.png
 # ├── app.py
 # └── requirements.txt
@@ -87,93 +87,77 @@ redis = redis.from_url(HEROKU_REDIS)
 ##################################
 # Route = /
 ##################################
-
 @app.route('/', methods=['GET','POST'])
-
 def main():
 
     # Front End Variables
     ##################################
-
     temp_img_url = '/static/welcome.png' # Welcome Image
-    # Display Thank You page after 100 attempts.
-    counter = image_data['counter']
-    if image_data["counter"] >= 31:
+    
+    # Set Globals
+    ##################################    
+    global USER
+    global COUNTER
+    
+    # Post
+    ##################################
+    if flask.request.method == 'POST': # Start the action
+        
+        ##################################
+        # Start App
+        ##################################
+        print(f'{USER} in redis data') # look for this in logs.
+        
+        # Input from user
+        inputs = flask.request.form
+        recorded_result = inputs['submit']
+        dummytime = datetime.now().strftime("%S") # This query string added to the URL tricks it into reloading.
+        temp_img_url = '/static/temp.png?dummy=' + str(dummytime) # display new image.
+        
+        # Magic
+        if COUNTER == 0:
+            generate_image()
+            COUNTER += 1
+            redis.set('counter', COUNTER) # Set Redis Token
+            redis.set('user', USER) # Set Redis Token
+        else:
+            process_answer(recorded_result) # Answer Function
+            write_data()
+            generate_image()
+            COUNTER += 1
+            redis.set('counter', COUNTER) # Set Redis Token
+            redis.set('user', USER) # Set Redis Token
+    else:
+        print('Not Post')
+    
+    # Page Selector
+    ##################################
+    enough_submissions = int(redis.get('counter'))
+    if enough_submissions > 30:
+        reset_app()
         url = 'thankyou.html'
     else:
         url = 'index.html'
-    
-
         
-    
-    # Response Capturing
-    ##################################
-
-    if flask.request.method == 'POST':
-        inputs = flask.request.form
-        recorded_result = inputs['submit']
-        dummytime = datetime.now().strftime("%S")
-        temp_img_url = '/static/temp.png?dummy=' + str(dummytime) # display new image.
-        answer(recorded_result) # Answer Function
-        # Session from Redis
-        if 'user' in session:
-            print(f'{USER} in redis data')
-            image_data['user'] = redis.get('user')
-            
-        else:
-            print(f'{USER} not in redis data')
-            redis.set('user', USER)
-            redis.set('counter', 0)
-            
-    else:
-        print('Response Capturing else')
-    
     # Render Template
-    return flask.render_template(url, counter = counter, temp_img_url = temp_img_url) 
+    ##################################
+    return flask.render_template(url, counter = int(redis.get('counter')), temp_img_url = temp_img_url) 
 
 ##################################
 # Global Variables
 ##################################
-
 USER = secrets.token_urlsafe(4) # User to store in the session.
 redis.set('user', USER) # Set Redis Token
 COUNTER = 0
 redis.set('counter', COUNTER) # Set Redis Token
-PALLET_SELECTED = 0 # User to store in the session. 
+PALLET_ITER = 0 # Start with pallet 0. 
+RANDOM_SPREAD = 15
 BACKGROUND = (255, 255, 255) # White background.
 TOTAL_CIRCLES = 700 # set to limit generation time.
 
-##################################
-# Ishihara Data
-##################################
-
-# Data dictionary pushed to SQL/CSV
-image_data = {
-    'user': USER,
-    'counter' : COUNTER,
-    'correct': 0,
-    'near_miss' : 0,
-    'recorded_result' : "recorded_result",
-    'mask_image' : "mask_image",
-    'cb_type1' : 0,
-    'cb_type2' : 0,
-    'ncb' : 0,
-    'datetime' : "datetime",
-    'random_spread' : 15,
-    'pallet_used' : "pallet_used",
-    'baseline' : [],
-    'ishihara_list' :[],
-    'COLORS_ON' : [],
-    'COLORS_OFF' : [],
-}
-
-##################################
 # Pallets
-##################################
-
-# Open and read the pallets csv
-pallets_dictionary = pd.read_csv('./static/pallets_dictionary.csv', index_col=False) # local
-# pallets_dictionary = pd.read_sql('colour_pallets', engine, index_col='pallet_name') # remote
+PALLETS_DICT = pd.read_csv('./static/pallets_dictionary.csv', index_col=['pallet_name']) # local
+# PALLETS_DICT = pd.read_sql('colour_pallets', engine, index_col='pallet_name') # remote
 
 ##################################
 # Randomisation of Colours
@@ -181,34 +165,59 @@ pallets_dictionary = pd.read_csv('./static/pallets_dictionary.csv', index_col=Fa
 
 # Each collumn is a new pallet
 def pallet_selector():
-    global PALLET_SELECTED
-    if PALLET_SELECTED > 16: # 16 pallets
-        PALLET_SELECTED = 0       
-    selected_pallet = pallets_dictionary.iloc[PALLET_SELECTED]
+    global PALLET_ITER
+    if PALLET_ITER > 16: # 16 pallets
+        PALLET_ITER = 0       
+    selected_pallet = PALLETS_DICT.iloc[PALLET_ITER]
     pallet_name = selected_pallet.name
-    PALLET_SELECTED += 1
-    return selected_pallet, pallet_name    
+    PALLET_ITER += 1
+    return selected_pallet, pallet_name # 1,2
 
 # Selecting a pallet and randomizing.
 def pallet_randomiser():
-    selected_pallet, pallet_name = pallet_selector()
-    image_data['ishihara_list'] = []  # reset the colours
+    selected_pallet, pallet_name = pallet_selector() # 1,2
+    randomised_pallet = []  # reset the colours
     for colour in selected_pallet[:12]:
         hex_colour = colour.lstrip('#')
         converted_value = list(int(hex_colour[i:i+2], 16) for i in (0, 2, 4)) # Hex conversion
-        colour_randomised = [np.random.randint((max(0, channel - image_data['random_spread'])), \
-                                               (min(255, channel + image_data['random_spread'])) \
+        colour_randomised = [np.random.randint((max(0, channel - RANDOM_SPREAD)), \
+                                               (min(255, channel + RANDOM_SPREAD)) \
                                               ) for channel in converted_value]
-        image_data['ishihara_list'].append(tuple(colour_randomised))
+        randomised_pallet.append(tuple(colour_randomised))
+    return selected_pallet, pallet_name, randomised_pallet # 3
 
-    image_data['COLORS_ON'] = [i  for i in image_data['ishihara_list'][0:6]] # Set the colours on the symbol
-    image_data['COLORS_OFF'] = [i for i in image_data['ishihara_list'][6:12]] # Set the background colours
-    image_data.update(pallet_used = pallet_name)  # update the pallet name
-    image_data.update(baseline = list(selected_pallet[:12]))  # (numpy) record baseline colours
-    image_data.update(cb_type1 = selected_pallet[12])
-    image_data.update(cb_type2 = selected_pallet[13])
-    image_data.update(ncb = selected_pallet[14])
+def mask():
+    masks = ["A","B","C","D","E","1","2","3","4","5","No Image"]
+    random_mask = random.choice(masks)
+    mask_path = './static/masks/'+random_mask+".png"
+    return random_mask, mask_path # 4,5
 
+##################################
+# Data Preperation
+##################################
+
+def prepare_data():
+
+    selected_pallet, pallet_name, randomised_pallet = pallet_randomiser() # 1,2,3
+    random_mask, mask_path = mask() # 4,5
+    
+    # Write to data
+    global image_data
+    image_data = {
+        'mask_path' : mask_path,
+        'mask_image' : random_mask, # 1
+        'cb_type1' : selected_pallet[12],
+        'cb_type2' : selected_pallet[13],
+        'ncb' : selected_pallet[14], # 1
+        'datetime' : datetime.now().strftime("%Y-%m-%d %H:%M:%S"), # auto
+        'random_spread' : RANDOM_SPREAD, # default
+        'pallet_used' : pallet_name, # 3
+        'baseline' : list(selected_pallet[:12]),
+        'colour_list' : randomised_pallet,
+        'COLORS_ON' : [i  for i in randomised_pallet[0:6]],
+        'COLORS_OFF' : [i for i in randomised_pallet[6:12]],
+    }
+    return image_data
 
 # Circle / Dot Functions
 # Adapted from https://github.com/franciscouzo/ishihara_generator
@@ -217,12 +226,10 @@ def pallet_randomiser():
 # max_diameter * 2.6 + min_diameter * 0.6
 def generate_circle(image_width, image_height, min_diameter, max_diameter):
     radius = random.triangular(min_diameter, max_diameter,max_diameter * .8 + min_diameter * .2) / 2
-
     angle = random.uniform(0, math.pi * 2)
     distance_from_center = random.uniform(0, image_width * 0.48 - radius)
     x = image_width  * 0.5 + math.cos(angle) * distance_from_center
     y = image_height * 0.5 + math.sin(angle) * distance_from_center
-
     return x, y, radius
 
 def overlaps_motive(image, xyr_values):
@@ -233,7 +240,6 @@ def overlaps_motive(image, xyr_values):
     for xy in zip(points_x, points_y):
         if image.getpixel(xy)[:3] != BACKGROUND:
             return True
-
     return False
 
 def circle_intersection(xyr_values1, xyr_values2):
@@ -255,13 +261,9 @@ def circle_draw(draw_image, image, xyr_values3):
 
 def generate_image():
     
-    pallet_randomiser()
-    masks = ["A","B","C","D","E","1","2","3","4","5","No Image"]
-    random_mask = random.choice(masks)
-    image_data.update(mask_image = random_mask) # WRITING TO IMAGE_DATA
-    mask_path = './static/masks/'+random_mask+".png"
-
-    image = Image.open(mask_path)
+    prepare_data()
+    
+    image = Image.open(image_data['mask_path'])
     image2 = Image.new('RGB', image.size, BACKGROUND)
     draw_image = ImageDraw.Draw(image2)
 
@@ -274,7 +276,7 @@ def generate_image():
     circle = generate_circle(width, height, min_diameter, max_diameter)
     circles = [circle]
 
-    circle_draw(draw_image, image, circle)
+    circle_draw(draw_image, image, circle,)
 
     try:
         for i in range(TOTAL_CIRCLES):
@@ -301,59 +303,86 @@ def generate_image():
             circle_draw(draw_image, image, circle)
     except (KeyboardInterrupt, SystemExit):
         pass
-
-    image2.save('static/temp.png') #overwrites existing image
-    thetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    image_data.update(datetime = thetime)
+    thetime =  image_data['datetime']
     # image2.save('static/records/' + str(thetime) + '.png') # Only used locally
-
+    image2.save('static/temp.png') #overwrites existing image
+    
 
 ##################################
 # Result Variables
 ##################################
 
-def answer(recorded_result):
+def process_answer(recorded_result):
+    global user_data
+    user_data = {
+        'user': str(redis.get('user')),
+        'counter' : int(redis.get('counter')),
+        'correct': 0,
+        'near_miss' : 0,
+        'recorded_result' : "recorded_result",
+    }
     
-    ### 1) Update Response
-    image_data.update(recorded_result = recorded_result)
-    ### 2) Evaluate Response
-    if image_data['recorded_result'] == image_data['mask_image']:
-        image_data.update(correct = 1) # WRITING TO IMAGE_DATA
+    # Update Response
+    user_data.update(recorded_result = recorded_result)
+    if user_data['recorded_result'] == image_data['mask_image']:
+        user_data.update(correct = 1) 
     else:
-        image_data.update(correct = 0) # WRITING TO IMAGE_DATA
-    
-    ### 3) Evaluate near-misses
+        user_data.update(correct = 0)
+        
+    # Evaluate near-misses
     near_miss_scenarios = [("E","B"),("D","B"),("3","B"),("C","5")]
-    near_miss_situation = (image_data['recorded_result'],image_data['mask_image'],)
-    if near_miss_situation in near_miss_scenarios or near_miss_situation[::-1] in near_miss_scenarios :
-        image_data.update(near_miss = 1)
+    near_miss_situation = (user_data['recorded_result'],image_data['mask_image'],)
+    if near_miss_situation in near_miss_scenarios or near_miss_situation[::-1] \
+    in near_miss_scenarios :
+        user_data.update(near_miss = 1)
     else:
-        image_data.update(near_miss = 0)
+        user_data.update(near_miss = 0)
 
-    ### 4) Turn the Counter +1
-    image_data["counter"] += 1 # WRITING TO IMAGE_DATA
-    redis.set('counter', image_data["counter"]) # Set Redis Token
 
-    ### 5) Prepare Data for SQL
-    datafile = pd.DataFrame(image_data.values(), index=image_data.keys()).T
-    datafile[['counter','correct','near_miss','cb_type1','cb_type2','ncb','random_spread']] = \
-    datafile[['counter','correct','near_miss','cb_type1','cb_type2','ncb','random_spread']].astype(int) # Get rid of numpy values for SQLAlchemy
-    datafile = datafile.reindex(sorted(datafile.columns, reverse=True), axis=1) # Sort columns reverse aphabetically.
-    datafile = datafile.drop(['COLORS_ON','COLORS_OFF'],axis=1) # unnecessary to store in db
-    user_result = datafile.drop(['baseline','datetime','ishihara_list','random_spread'],axis=1)
+# Annoying bug with SQL Alchemy numpy.int64
+def final_cleaning():
+    global image_data
+    global user_data
     
-    ### 6) Push Data
-    if str(image_data['user']) == redis.get('user'):
+    # Build single file
+    datafile = pd.DataFrame(image_data.values(), index=image_data.keys()).T
+    userfile = pd.DataFrame(user_data.values(), index=user_data.keys()).T
+    resultfile = pd.concat([datafile,userfile], axis=1)
+    
+    # Prepare Data for SQL
+    resultfile[['counter','correct','near_miss','cb_type1','cb_type2','ncb','random_spread']] = \
+    resultfile[['counter','correct','near_miss','cb_type1','cb_type2','ncb','random_spread']].astype(int) # Get rid of numpy values for SQLAlchemy
+    resultfile = resultfile.reindex(sorted(resultfile.columns, reverse=True), axis=1) # Sort columns reverse aphabetically.
+    resultfile = resultfile.drop(['mask_path','COLORS_ON','COLORS_OFF'],axis=1) # drop unnecessary data
+    user_result = resultfile.drop(['baseline','datetime','colour_list','random_spread'],axis=1) # drop unnecessary data
+    
+    return resultfile, user_result
+
+# Push data to file/SLQ
+def write_data():
+    resultfile, user_result = final_cleaning()
+    # Push Data to file/sql
+    if str(user_data['user']) == str(redis.get('user')):
         action = 'append'
     else:
         action = 'replace'
-#     user_result.to_sql('colour_results', engine, if_exists=action, index=False) # need a good mechanism here, based on user 
-#     datafile.to_sql('colour_data', engine, if_exists='append', index=False) 
-    datafile.to_csv('./Notebooks/CSV/dev_colourdata.csv', header=True, index=False)
-
-    ### 7) Generate New Image
-    generate_image() # Run Main Func
-
+    # SQL
+#     user_result .to_sql('colour_results', engine, if_exists=action, index=False) # need a good mechanism here, based on user 
+#     resultfile.to_sql('colour_data', engine, if_exists='append', index=False) 
+    # Local
+    user_result.to_csv('./Notebooks/CSV/local_colour_results.csv', mode='a', header=False, index=False)
+    resultfile.to_csv('./Notebooks/CSV/local_colour_data.csv', mode='a', header=False, index=False)
+    
+def reset_app():
+    global USER
+    global COUNTER
+    redis.flushdb()
+    USER = secrets.token_urlsafe(4) # User to store in the session.
+    redis.set('user', USER) # Set Redis Token
+    COUNTER = 0
+    redis.set('counter', COUNTER) # Set Redis Token
+    pass
+    
 ##################################
 # Image Refresh 
 ##################################
@@ -373,5 +402,3 @@ if __name__ == '__main__':
     # can be configured by adding an `entrypoint` to app.yaml.
     app.run(host='127.0.0.1', port=5000, use_reloader=True)
 # [END gae_python38_app]
-
-
